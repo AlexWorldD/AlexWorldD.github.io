669cd70738fb43fb8a0a178aabaab6a449df824c
Alex Malyutin <malutin.alex@gmail.com>
2017-09-22 00:34:20 +0300

    Split quote to author and data.

--- hw1/js/scripts.js
+++ hw1/js/scripts.js
@@ -1,16 +1,25 @@
 'use strict';
-CustomEvent
 var current_q = 0;
 var quotes = [
-    "All successful men and women are big dreamers. They imagine what their future could be, ideal in every respect, and then they work every day toward their distant vision, that goal or purpose.” – Brian Tracy"
-    , "Nothing is impossible, the word itself says 'I'm possible'!” – Audrey Hepburn"
-    , "Paris is always a good idea” – Audrey Hepburn"];
+    {
+        "data": "All successful men and women are big dreamers. They imagine what their future could be, ideal in every respect, and then they work every day toward their distant vision, that goal or purpose.”"
+        , "author": "Brian Tracy"
+    }
+    , {
+        "data": "Nothing is impossible, the word itself says 'I'm possible'!”"
+        , "author": "Audrey Hepburn"
+    }
+    , {
+        "data": "Paris is always a good idea”"
+        , "author": "Audrey Hepburn"
+    }];
 
 function change() {
     var tmp = Math.floor(Math.random() * quotes.length);
-    while (current_q == tmp) {
+    while (current_q === tmp) {
         tmp = Math.floor(Math.random() * quotes.length);
     }
-    document.getElementById("quote").innerHTML = quotes[tmp];
+    document.getElementById("quote").innerHTML = quotes[tmp].data;
+    document.getElementById("author").innerHTML = quotes[tmp].author;
     current_q = tmp;
 }
