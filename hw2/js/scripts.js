dd74dde8427133a75e3218fc9dc9016db419860f
Alex Malyutin <malutin.alex@gmail.com>
2017-10-12 20:23:39 +0300

    Start new HW

--- hw2/js/scripts.js
+++ hw2/js/scripts.js
@@ -1,93 +1,103 @@
-'use strict';
-var quotesPath = "https://raw.githubusercontent.com/4skinSkywalker/Database-Quotes-JSON/master/quotes.json"
-    //var quotesPath = 'https://www.w3schools.com/js/json_demo.txt'
-var current_q = 0;
-var quotes = [
-    {
-        "data": "“All successful men and women are big dreamers. They imagine what their future could be, ideal in every respect, and then they work every day toward their distant vision, that goal or purpose.”"
-        , "author": "Brian Tracy"
-    }
-    , {
-        "data": "“Nothing is impossible, the word itself says 'I'm possible'!”"
-        , "author": "Audrey Hepburn"
-    }
-    , {
-        "data": "“Paris is always a good idea”"
-        , "author": "Audrey Hepburn"
-    }
-    , {
-        "data": "“Having nothing, nothing can he lose”"
-        , "author": "William Shakespeare"
-    }];
+//'use strict';
+// alpha2_code
+//     :
+//     "sy"
+// continent
+//     :
+//     "Asia"
+// gdp
+//     :
+//     0
+// latitude
+//     :
+//     33.5146
+// life_expectancy
+//     :
+//     74.7107073170732
+// longitude
+//     :
+//     36.3119
+// name
+//     :
+//     "Syrian Arab Republic"
+// population
+//     :
+//     14338240
+// year
+//     :
+//     2012
 
-function change() {
-    var tmp = Math.floor(Math.random() * quotes.length);
-    while (current_q === tmp) {
-        tmp = Math.floor(Math.random() * quotes.length);
-    }
-    document.getElementById("quote").innerHTML = quotes[tmp].data;
-    document.getElementById("author").innerHTML = quotes[tmp].author;
-    current_q = tmp;
-}
+var required_columns = ['Name', 'Continent', 'GDP', 'Life Expectancy', 'Population', 'Year']
+var def_titles = ['name', 'continent', 'gdp', 'life_expectancy', 'population', 'year']
+d3.json("data/countries_2012.json", function (error, data) {
 
-function change_v2() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            var data = JSON.parse(this.responseText);
-            var tmp = Array.from({
-                length: 5
-            }, () => Math.floor(Math.random() * 4000));
-            document.getElementById("quote").innerHTML = data[tmp[4]].quoteText;
-            document.getElementById("author").innerHTML = data[tmp[4]].quoteAuthor;
-            var q_list = document.getElementsByClassName("q_text");
-            var a_list = document.getElementsByClassName("q_author");
-            for (var t_t=0; t_t<4; t_t++) {
-                q_list[t_t].innerHTML = data[tmp[t_t]].quoteText;
-                a_list[t_t].innerHTML = data[tmp[t_t]].quoteAuthor;
-            } 
-        }
-    };
-    xhr.open("GET", quotesPath, true);
-    xhr.send();
-}
+    var columns = Object.keys(data[0]);
+    var sortAscending = true;
+    // Build a table. ~Empty table~
+    var table = d3.select(".content").append("table"),
+        thead = table.append("thead")
+            .attr("class", "thead");
+    tbody = table.append("tbody");
 
-function change_v3() {
-    document.getElementById("AuthorList").classList.toggle("show");
-}
+    table.append("caption")
+        .html("World Countries Ranking");
 
-function sort_q() {
-    var sort_me = [];
-    var q_list = Array.from(document.getElementsByClassName("q_text"));
-    var a_list = document.getElementsByClassName("q_author");
-    // Build Dictionary here ->
-    q_list.forEach((key, idx) => sort_me.push({"quote": key.innerHTML, "author":  a_list[idx].innerHTML}));
-    // Sorting
-    sort_me.sort(function (a, b) {
-        if (a.quote > b.quote) {
-            return 1;
-        }
-        else if (a.quote < b.quote) {
-            return -1;
-        }
-        return 0;
-    });
-    var q_list = document.getElementsByClassName("q_text");
-    var a_list = document.getElementsByClassName("q_author");
-    for (var t_t=0; t_t<4; t_t++) {
-                q_list[t_t].innerHTML = sort_me[t_t].quote
-                a_list[t_t].innerHTML = sort_me[t_t].author;
+    // Putting our data to table
+    var headers = thead.append("tr").selectAll("th")
+        .data(columns)
+        .enter()
+        .append("th")
+        .text(function (d) {
+            return d;
+        })
+        .on("click", function (header) {
+            alert(header);
+            headers.attr('class', 'header');
+            if (sortAscending) {
+                tbody.selectAll("tr").sort(function (a, b) {
+                    sortAscending = false;
+                    d3.select(header).attr('class', 'aes');
+                    return d3.ascending(a[header], b[header]);
+                });
             }
-}
-window.onclick = function (event) {
-    if (!event.target.matches('.change-btn_V3')) {
-        var dropdowns = document.getElementsByClassName("dropdown_content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
+            else {
+                tbody.selectAll("tr").sort(function (a, b) {
+                sortAscending=true;
+                this.className='des';
+                return d3.descending(a[header], b[header]);
+
+            });
+        }});
+
+    var rows = tbody.selectAll("tr.row")
+        .data(data)
+        .enter()
+        .append("tr").attr("class", "row");
+
+    var cells = rows.selectAll("td")
+        .data(function (row) {
+            return d3.range(Object.keys(row).length).map(function (column, i) {
+                return row[Object.keys(row)[i]];
+            });
+        })
+        .enter()
+        .append("td")
+        .text(function (d) {
+            return d;
+        })
+        .on("mouseover", function (d, i) {
+
+            d3.select(this.parentNode)
+                .style("background-color", "#F3ED86");
+
+        }).on("mouseout", function () {
+
+            tbody.selectAll("tr")
+                .style("background-color", null)
+                .selectAll("td")
+                .style("background-color", null);
+
+        });
+
+})
+;
