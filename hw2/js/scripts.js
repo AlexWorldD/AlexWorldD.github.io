4766d32d3c1838b685575a87027cc23fefe97945
Alex Malyutin <malutin.alex@gmail.com>
2017-10-14 17:27:15 +0300

    ..

--- hw2/js/scripts.js
+++ hw2/js/scripts.js
@@ -73,6 +73,7 @@ d3.json("data/countries_2012.json", function (error, data) {
         .text(function (d) {
             return d.head;
         });
+
     // Build an empty table with headers
     headers.on("click", function (header) {
         headers.attr('class', 'header');
@@ -101,6 +102,7 @@ d3.json("data/countries_2012.json", function (error, data) {
             this.className = 'des';
         }
     });
+
     // Start putting our data to table
     var rows = tbody.selectAll("tr.row")
         .data(data)
@@ -108,7 +110,10 @@ d3.json("data/countries_2012.json", function (error, data) {
         .append("tr").attr("class", "row");
 
     var cells = rows
-        .appendMany(td_data, 'td')
+        .selectAll('td')
+        .data(td_data)
+        .enter()
+        .append('td')
         .html(d3.f('html'))
         .attr('class', d3.f('cl'));
 
@@ -120,12 +125,12 @@ d3.json("data/countries_2012.json", function (error, data) {
 
         }).on("mouseout", function () {
 
-            tbody.selectAll("tr")
-                .style("background-color", null)
-                .selectAll("td")
-                .style("background-color", null);
+        tbody.selectAll("tr")
+            .style("background-color", null)
+            .selectAll("td")
+            .style("background-color", null);
 
-        });
+    });
 
     function td_data(row, i) {
         return columns1.map(function (c) {
@@ -147,8 +152,7 @@ d3.json("data/countries_2012.json", function (error, data) {
             var temp = d3.select(this);
             if (temp.property("checked")) {
                 choices.push(temp.property("value"));
-            }
-            ;
+            };
         });
         var newData;
         if (choices.length > 0) {
@@ -159,29 +163,59 @@ d3.json("data/countries_2012.json", function (error, data) {
         else {
             newData = req_data;
         }
+        var update = function (new_data) {
+            // Row selection for update
+            n_rows = tbody.selectAll('tr.row').data(new_data);
+            n_rows.exit()
+                .transition()
+                .delay(0)
+                .duration(0)
+                .style('opacity', 0.0)
+                .remove();
+            n_rows  = n_rows.enter()
+                .append("tr").attr("class", "row").merge(n_rows);
 
-        tbody.selectAll("tr.row").html('');
-        var newRows = tbody.selectAll("tr.row")
-            .data(newData);
-        newRows
-            .enter()
-            .append("tr").attr("class", "row");
+            n_cells = n_rows
+                .selectAll('td')
+                .data(td_data);
+            n_cells.exit()
+                .transition()
+                .delay(0)
+                .duration(0)
+                .style('opacity', 0.0)
+                .remove();
+            n_cells = n_cells
+                .enter()
+                .append('td')
+                .html(d3.f('html'))
+                .attr('class', d3.f('cl'))
+                .merge(n_cells);
 
-        newRows.exit().remove();
-        rows = newRows;
-        var cells = rows
-            .appendMany(td_data, 'td')
-            .html(d3.f('html'))
-            .attr('class', d3.f('cl'));
+        }
 
-        var newCells = rows.data(td_data);
-        newCells
-            .enter()
-            .append('td')
-            .html(d3.f('html'))
-            .attr('class', d3.f('cl'));
-        newCells.exit().remove();
-        cells = newCells;
+        update(newData);
+        // tbody.selectAll("tr.row").html('');
+        // var newRows = tbody.selectAll("tr.row")
+        //     .data(newData);
+        // newRows
+        //     .enter()
+        //     .append("tr").attr("class", "row");
+        //
+        // newRows.exit().remove();
+        // rows = newRows;
+        // var cells = rows
+        //     .appendMany(td_data, 'td')
+        //     .html(d3.f('html'))
+        //     .attr('class', d3.f('cl'));
+        //
+        // var newCells = rows.data(td_data);
+        // newCells
+        //     .enter()
+        //     .append('td')
+        //     .html(d3.f('html'))
+        //     .attr('class', d3.f('cl'));
+        // newCells.exit().remove();
+        // cells = newCells;
         // cells.appendMany(td_data, 'td')
         //     .html(d3.f('html'))
         //     .attr('class', d3.f('cl'));
