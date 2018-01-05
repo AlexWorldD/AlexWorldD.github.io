b084bd035f210c18e641c0ba52dff64f542af366
Alex Malyutin <malutin.alex@gmail.com>
2018-01-06 00:26:13 +0300

    Start 4.1, simple list - done.

--- hw4/p1/js/scripts.js
+++ hw4/p1/js/scripts.js
@@ -1,48 +1,56 @@
 //Global vars
 let graph = {
     nodes: [],
-    edges: []
+    links: []
 };
 let svg = d3.select('#graph');
-let width = +d3.select('#graph').attr('width');
-let height = +d3.select('#graph').attr('height');
-let center = {x: window.innerWidth / 2, y: height / 2}
+let margin = {top: 20, bottom: 10, left: 10, right: 10};
+let width = 1280 - margin.left - margin.right;
+let height = 1280 - margin.top - margin.bottom;
+svg
+    .attr('width', width)
+    .attr('height', height);
+let center = {x: window.innerWidth / 2, y: height / 2};
 let simulation = d3.forceSimulation()
-    .force('link', d3.forceLink().id(function (d) {
-        return d.id;
-    }))
     .force('charge', d3.forceManyBody().strength(-100))
-    .force('center', d3.forceCenter(center.x, center.y))
-    .force('x', d3.forceX().strength(0.5).x(center.x))
-    .force('y', d3.forceY().strength(0.5).y(center.y));
+    .force('x', d3.forceX().strength(0.2).x(center.x))
+    .force('y', d3.forceY().strength(0.2).y(center.y));
+// .force('link', d3.forceLink().id(function (d) {
+//     return d.id;
+// }))
+// .force('charge', d3.forceManyBody().strength(-100))
+// .force('center', d3.forceCenter(center.x, center.y))
 
+let node_radius = 7;
 d3.json('data/countries_1995_2012.json', function (error, data) {
     if (error) throw error;
-    // Okay, we didn't have any issues getting our data...
 
     // Getting our data for building pretty graph
     graph.nodes = data_prepare(data);
     graph.nodes.forEach(t => {
         t.Partners.forEach(
             _t => {
-                graph.edges.push({
+                let target = graph.nodes.find(x => x.id === _t.country_id);
+                if (target == undefined) {
+                    console.log(t.Name);
+                }
+                graph.links.push({
                     'source': t,
-                    'target': graph.nodes.find(x => x.id == _t.country_id),
+                    'target': target,
                     'value': _t.total_export
                 })
             }
         )
     });
-    // console.log(graph.edges);
 
-    let edgeLayer = svg.append('g')
+    let linkLayer = svg.append('g')
         .attr('class', 'links');
     // Now let's create the lines
-    let links = edgeLayer.selectAll('.edge')
-        .data(graph.edges)
-        .enter().append('line')
-        .attr('stroke-width', 1);
-        // .attr('stroke-width', d => Math.sqrt(d.value));
+    let links = linkLayer.selectAll('.link')
+        .data(graph.links)
+        .enter()
+        .append('line');
+    // .attr('stroke-width', d => Math.sqrt(d.value));
     let nodeLayer = svg.append('g')
         .attr('class', 'nodes');
     let nodes = nodeLayer
@@ -50,6 +58,7 @@ d3.json('data/countries_1995_2012.json', function (error, data) {
         .data(graph.nodes)
         .enter()
         .append('g')
+        .classed('node', true)
         .call(d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
@@ -64,13 +73,28 @@ d3.json('data/countries_1995_2012.json', function (error, data) {
         .attr("class", 'label_node');
     nodes.append('circle')
         .attr("class", "dot_node")
-        .attr("r", 5);
+        .attr('r', node_radius);
 
-    console.log(graph);
+    function graph_update(duration) {
 
-    simulation.nodes(graph.nodes);
+        links.transition().duration(duration)
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
+
+        nodes.transition().duration(duration)
+            .attr("transform", function (d, i) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+    }
+
+    simulation.nodes(graph.nodes)
+        .on('tick', graph_update(0));
     // simulation.force("link")
-    //     .links(graph.edges);
+    //     .links(graph.links);
+
+    draw_list();
 
     // TODO refactoring below>>>
     // simulation.on("tick", _ => {
@@ -85,9 +109,70 @@ d3.json('data/countries_1995_2012.json', function (error, data) {
     //     // text
     //     //     .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
     // });
+    function draw_list() {
+        simulation.stop();
+        graph.nodes.forEach((d, i) => {
+            d.x = 20;
+            d.y = node_radius * 3 * i;
+        });
+        let new_height = node_radius*graph.nodes.length*3+100;
+        svg
+            .attr('height', new_height);
+        graph_update(999);
 
+    }
 });
 
+
+
+// Additional functions
+// Table-Bar switcher
+d3.selectAll('input[type=radio][name="mode"]').on("change", update_filters);
+
+function update_filters() {
+    let cur_page = d3.select('input[name="mode"]:checked').node().value;
+    if (cur_page === 'mix') {
+        d3.select('#mix_filters').style("display", "block");
+    }
+    else {
+        d3.select('#mix_filters').style("display", "none");
+    }
+    update_vis();
+}
+
+function update_vis() {
+    let cur_page = d3.select('input[name="mode"]:checked').node().value;
+    switch (cur_page) {
+        case "list":
+            draw_list();
+    }
+}
+
+// Function for preparing Data according natural language titles of columns
+function data_prepare(data) {
+    return data.map(function (t) {
+            // Take data just for specific data... for example,. 2010
+            let _cur = t.years.find(_t => _t.year === 2012);
+            return {
+                'Name': t.name,
+                'id': t.country_id,
+                'Code': t.alpha2_code,
+                'Continent': t.continent,
+                'Latitude': t.latitude,
+                'Longitude': t.longitude,
+                'GDP': _cur.gdp,
+                'Life Expectancy': _cur.life_expectancy,
+                'Population': _cur.population,
+                'Partners': _cur.top_partners,
+                //    Specific vars for graph structure:
+                // 'x': window.innerWidth / 4 + 2 * window.innerWidth * Math.random() / 4,
+                // 'y': height / 4 + 2 * height * Math.random() / 4
+                'x': 0,
+                'y': 0
+            }
+        }
+    )
+}
 
 // Ctrl+C & Ctrl+V from https://codepen.io/_avt_/pen/rGQQPw?editors=1000
 function dragstarted(d) {
@@ -107,28 +192,4 @@ function dragended(d) {
     d.fy = null;
 }
 
-// Function for preparing Data according natural language titles of columns
-function data_prepare(data) {
-    return data.map(function (t) {
-            // Take data just for specific data... for example,. 2010
-            let _cur = t.years.find(_t => _t.year === 2010);
-            return {
-                'Name': t.name,
-                'id': t.country_id,
-                'Code': t.alpha2_code,
-                'Continent': t.continent,
-                'Latitude': t.latitude,
-                'Longitude': t.longitude,
-                'GDP': _cur.gdp,
-                'Life Expectancy': _cur.life_expectancy,
-                'Population': _cur.population,
-                'Partners': _cur.top_partners,
-                //    Specific vars for graph structure:
-                'x': window.innerWidth / 4 + 2 * window.innerWidth * Math.random() / 4,
-                'y': height / 4 + 2 * height * Math.random() / 4
-                // 'x': 0,
-                // 'y': 0
-            }
-        }
-    )
-}
+
